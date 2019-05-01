7a999bba0958274854b70f8680c6bfb606d95d31
Joshua Lopez-Roque <joshualopezroque@gmail.com>
2019-04-30 21:05:42 -0700

    Updates

---
+++ precache-manifest.5abf00b70795524681f9f931d26136cd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94b0d8b6885cfa3fbe1e482a58a5d361",
+    "url": "/theGame/index.html"
+  },
+  {
+    "revision": "f4368e99d0d4a979a49b",
+    "url": "/theGame/static/css/main.1add54f2.chunk.css"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/theGame/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "f4368e99d0d4a979a49b",
+    "url": "/theGame/static/js/main.46dd751d.chunk.js"
+  },
+  {
+    "revision": "890fa21ffa6443aa40bf",
+    "url": "/theGame/static/js/runtime~main.019f1ab3.js"
+  }
+]);
